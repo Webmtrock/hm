e708ebf79fbbcce68a1b9f4c32f9054daaf555f7
webrock Media <webrockmedia@webrocks-MacBook-Pro.local>
2024-04-03 03:47:59 -0500

    testing

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,21 @@
+/*
+  Authors : initappz (Rahul Jograna)
+  Website : https://initappz.com/
+  App Name : Grocery Delivery App
+  This App Template Source code is licensed as per the
+  terms found in the Website https://initappz.com/license
+  Copyright and Good Faith Purchasers © 2021-present initappz.
+*/
+export const environment = {
+  production: true,
+  baseUrl: 'http://127.0.0.1:8000/api/',
+  imageUrl: 'http://127.0.0.1:8000/storage/images/',
+  firebase: {
+    apiKey: "YOURKEYS",
+    authDomain: "YOURKEYS",
+    projectId: "YOURKEYS",
+    storageBucket: "YOURKEYS",
+    messagingSenderId: "YOURKEYS",
+    appId: "YOURKEYS"
+  }
+};
